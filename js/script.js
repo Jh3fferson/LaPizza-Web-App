01d6ae5c50bafc0922bc5f5e5c8e766aa233bb80
Jh3fferson <jheffersonmatias123@gmail.com>
2025-05-30 02:34:04 -0300

    array nomeado incorretamente

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ function renderizarProdutos(categoria_numero = 0) {
   });
 
   extras.forEach((extra) => {
-    if (pizza.categoria_numero == categoria_numero || categoria_numero == 0) {
+    if (extra.categoria_numero == categoria_numero || categoria_numero == 0) {
     var productHtml = `
                   <div class="item-card">
                     <a data-id="${extra.id}" href="#" class="item item-extra">
